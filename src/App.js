c2205befbc4129409319ea132463053a9c0f607d
Peter <adelekepeterpierre@gmail.com>
2024-07-02 02:34:57 +0100

    added the calculation into the stats component

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -107,11 +107,24 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have x items on your list, and you already packed x%</em>
+      <em>
+        {percentage === 100
+          ? "You got everything ready to go ✈️"
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
-
